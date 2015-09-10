27d9a8eb8f89ce49e7e288b8fe8d32ea8abb2f07
Makers Laptop 22 <makerslaptop22@Makerss-MacBook-Pro-3.local>
2015-09-10 11:04:35 +0100

    Created sinatra server

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -14,9 +14,10 @@ describe('Github Profile finder', function() {
 
   it('finds profiles', function() {
 
-    searchBox.sendKeys('spike01');
+    searchBox.sendKeys('spike');
     searchButton.click();
 
-    expect(element(by.binding('user.login')).getText()).toEqual('spike01')
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.get(0).getText()).toEqual('spike');
   });
 });
